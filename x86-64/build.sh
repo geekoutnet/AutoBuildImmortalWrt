65d146080a035e28ab1e270709cd9396c70d2edd
ぃ慕容ぅ <13740941+geekoutnet@users.noreply.github.com>
2025-02-09 11:27:48 +0800

    Update build.sh

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -40,6 +40,9 @@ PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
+PACKAGES="$PACKAGES luci-app-openclash"
+PACKAGES="$PACKAGES luci-app-keepalived"
+
 
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
